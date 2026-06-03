36c4905f74b62562eee9c7346bb52b5ccff90d46
Elijah <elijah.jegede@mygeorgian.ca>
2026-06-02 23:36:29 -0400

    Add the critical alert to the status update

--- labs/lab-1/lab-2/js/main.js
+++ labs/lab-1/lab-2/js/main.js
@@ -1,5 +1,10 @@
-let health = 65;
+let health = 3;
 let ammo = 8;
 let shield = true;
 let missionProgress = 40;
 let enemyNearby = true;
+
+const statusUpdate = document.querySelector("#statusUpdate");
+if (health < 30 && enemyNearby){
+    statusUpdate.textContent = "CRITICAL ALERT: Immediate Evacuation Required";
+}
