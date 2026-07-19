3424dc3168d398dba8c3d998722e460ebbf05652
Elijah <elijah.jegede@mygeorgian.ca>
2026-07-18 23:42:23 -0400

    Created the Pizza class

--- assignments/assignment-2/js/main.js
+++ assignments/assignment-2/js/main.js
@@ -1,3 +1,26 @@
+// Pizza class
+class Pizza {
+    constructor(customerName, size, crust, toppings) {
+        this.customerName = customerName;
+        this.size = size;
+        this.crust = crust;
+        this.toppings = toppings;
+    }
+
+    // Returns a description of the pizza order
+    getDescription() {
+        let toppingList;
+
+        if (this.toppings.length === 0) {
+            toppingList = "no toppings";
+        } else {
+            toppingList = this.toppings.join(", ");
+        }
+
+        return `${this.customerName} ordered a ${this.size} ${this.crust} crust pizza with ${toppingList}.`;
+    }
+}
+
 document.getElementById("pizzaOrderForm").addEventListener("submit", function(event) {
 
     // Prevent the page from refreshing
